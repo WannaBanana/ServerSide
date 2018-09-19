a42a81d9577c121638a4936d7c36f1677ef886bb
TMineCola <wang.minecola@gmail.com>
2018-09-19 14:47:06 +0800

    fix fields verify

--- routes/register.js
+++ routes/register.js
@@ -3,18 +3,19 @@ var router = express.Router();
 
 /* 新增帳號 */
 router.post('/', function(req, res) {
-    let requsetObject = req.body;
-    let verify_fields = ["first_name", "last_name", "email", "password", "student_id", "cellphone"];
+    let requestObject = req.body;
+    console.log(requestObject);
+    let verify_fields = ["photo", "first_name", "last_name", "email", "password", "student_id", "cellphone"];
     let lack_fields = [];
     for(let key in verify_fields) {
-        if(!requsetObject.hasOwnProperty(key)) {
-            lack_fields.push(key);
+        if(!requestObject.hasOwnProperty(verify_fields[key])) {
+            lack_fields.push(verify_fields[key]);
         }
     }
-    if(lack_fields.length() == 0) {
-        res.status(200).send(requsetObject);
+    if(lack_fields.length == 0) {
+        res.status(200).send(requestObject);
     } else {
-        res.status(403).send('{ "message": "缺少欄位 ' + lack_fields + ' 資訊" }');
+        res.status(403).send(lack_fields.join(', '));
     }
 });
 
