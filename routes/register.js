d2e66a694740b08eb6826131fdfb9bf0be710d86
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-09-19 12:53:35 +0800

    新增欄位驗證

--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* 新增帳號 */
 router.post('/', function(req, res) {
     let requsetObject = req.body;
-    res.status(200).send(requsetObject);
+    let verify_fields = ["first_name", "last_name", "email", "password", "student_id", "cellphone"];
+    let lack_fields = [];
+    for(let key in verify_fields) {
+        if(!requsetObject.hasOwnProperty(key)) {
+            lack_fields.push(key);
+        }
+    }
+    if(lack_fields.length() == 0) {
+        res.status(200).send(requsetObject);
+    } else {
+        res.status(403).send('{ "message": "缺少欄位 ' + lack_fields + ' 資訊" }');
+    }
 });
 
 module.exports = router;
