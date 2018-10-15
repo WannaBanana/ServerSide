1870cd54e354bb1b16a4de5f249a0b93977a5659
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-10-15 21:56:52 +0800

    line bot testing

--- routes/linebot.js
+++ routes/linebot.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var router = express.Router();
 const line = require('@line/bot-sdk');
+const config = require('../ENV.json');
 
-/* 不知道 */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+const config = {
+    channelAccessToken: config.line_accessToken,
+    channelSecret: config.line_Secret
+};
+
+const client = new line.Client(config);
+
+router.post('/webhook', line.middleware(config), (req, res) => {
+    Promise.all(req.body.events.map(handleEvent)).then((result) => res.json(result));
 });
+
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        return Promise.resolve(null);
+    }
+
+    return client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: event.message.text
+    });
+}
 
 module.exports = router;
