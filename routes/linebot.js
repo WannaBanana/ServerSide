1dbdffd614f85a0f4657189f3ef582362b24e3df
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-10-16 19:08:17 +0800

    linebot testing

--- routes/linebot.js
+++ routes/linebot.js
@@ -1,8 +1,8 @@
 var express = require('express');
-
 var router = express.Router();
 var linebot = require('linebot');
-const config = require('../ENV.json');
+var bodyParser = require('body-parser');
+var config = require('./ENV.json');
 
 const bot = linebot({
     channelId: config.line_channelID,
@@ -10,16 +10,26 @@ const bot = linebot({
     channelAccessToken: config.line_accessToken
 });
 
-const linebotParser = bot.parser();
+const parser = bodyParser.json({
+    verify: function (req, res, buf, encoding) {
+        req.rawBody = buf.toString(encoding);
+    }
+});
 
-router.post('/', linebotParser);
+router.post('/', parser, function (req, res) {
+    if (!bot.verify(req.rawBody, req.get('X-Line-Signature'))) {
+        return res.sendStatus(400);
+    }
+    bot.parse(req.body);
+    return res.json({});
+});
 
 bot.on('message', function (event) {
-  event.reply(event.message.text).then(function (data) {
-    console.log('Success', data);
-  }).catch(function (error) {
-    console.log('Error', error);
-  });
+    event.reply(event.message.text).then(function (data) {
+        console.log('Success', data);
+    }).catch(function (error) {
+        console.log('Error', error);
+    });
 });
- 
+
 module.exports = router;
