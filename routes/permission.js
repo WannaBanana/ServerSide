bf85f9df6b4e70b55104859f25e0ae332235d5eb
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-10-29 03:55:59 +0800

    刪註解

---
+++ routes/permission.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/admin/:sid', function(req, res) {
+
+});
+
+router.post('/admin', function(req, res) {
+
+});
+
+router.get('/:sid', function(req, res) {
+
+});
+
+module.exports = router;
