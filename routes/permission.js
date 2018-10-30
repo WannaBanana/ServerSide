78b8bdefbdbe1d56e5a2c2b4da89bd8055ccf0d8
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-10-30 21:32:11 +0800

    新增CRUD permission

--- routes/permission.js
+++ routes/permission.js
@@ -40,4 +40,68 @@ router.get('/:sid', function(req, res) {
     });
 });
 
+/* 設定管理員 */
+router.post('/admin/:sid', function(req, res) {
+    let sid = req.params.sid;
+    ref = req.database.ref('/permission/' + sid + '/admin');
+    res.set(true).then(()=>{
+        res.status(200).send({"message": "管理員設定成功"});
+    });
+});
+
+/* 新增權限 */
+router.post('/:sid', function(req, res) {
+    let sid = req.params.sid;
+    let requestObject = req.body;
+    ref = req.database.ref('/permission/' + sid + '/space');
+    ref.once("value").then(function(snapshot) {
+        let permissionObject = snapshot.val();
+        if(permissionObject) {
+            for(let dep in requestObject) {
+                for(let space in requestObject[dep]) {
+                    permissionObject[dep].push(space);
+                }
+            }
+            ref.set(permissionObject).then(() => {
+                res.status(200).send({"message": "新增權限成功"});
+            });
+        } else {
+            ref.set(requestObject).then(() => {
+                res.status(200).send({"message": "新增權限成功"});
+            });
+        }
+    });
+});
+
+router.put('/:sid', function(req, res) {
+    let sid = req.params.sid;
+    let requestObject = req.body;
+    ref = req.database.ref('/permission/' + sid + '/space');
+    ref.set(requestObject);
+});
+
+/* 刪除權限 */
+router.delete('/:sid', function(req, res) {
+    let sid = req.params.sid;
+    let requestObject = req.body;
+    ref = req.database.ref('/permission/' + sid + '/space');
+    ref.once("value").then(function(snapshot) {
+        let permissionObject = snapshot.val();
+        if(permissionObject) {
+            for(let dep in requestObject) {
+                for(let space in requestObject[dep]) {
+                    permissionObject[dep].splice(permissionObject[dep].indexOf(space), 1);
+                }
+            }
+            ref.set(permissionObject).then(() => {
+                res.status(200).send({"message": "刪除權限成功"});
+            });
+        } else {
+            ref.set(requestObject).then(() => {
+                res.status(404).send({"message": "使用者無該權限"});
+            });
+        }
+    });
+});
+
 module.exports = router;
