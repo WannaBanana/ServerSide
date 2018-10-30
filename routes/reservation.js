89e999d47d6ba0c48db1e76fd0827d0cdf337560
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-10-30 22:49:58 +0800

    fixed

--- routes/reservation.js
+++ routes/reservation.js
@@ -16,7 +16,7 @@ router.get('/book/:department/:space', function(req, res) {
                 for(let key in reservationObject[space][date]) {
                     if(reservationObject[space][date][key].state == '未核准') {
                         responseObject[date] = {};
-                        responseObject[date][key] = reservationObject[space][date][key];
+                        responseObject[date][key] = reservationObject[date][key];
                     }
                 }
             }
