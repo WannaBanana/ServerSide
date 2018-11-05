86770391c9e0fe96d1d219655f317428a9b59efe
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-11-05 23:26:01 +0800

    新增 alert 回傳

--- routes/alert.js
+++ routes/alert.js
@@ -2,10 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
+    let responseObject = {};
     ref = req.database.ref('/alert');
     ref.once("value").then(function(snapshot) {
         let alertObject = snapshot.val();
-        res.status(200).send(alertObject);
+        for(let key in alertObject) {
+            let department = alertObject[key].source.slice(0,4);
+            let space = alertObject[key].source.slice(4);
+            let date = new Date(alertObject[key].time).toISOString().slice(0, 10);
+            if(!responseObject[department]) {
+                responseObject[department] = {};
+            }
+            if(!responseObject[department][space]) {
+                responseObject[department][space] = {};
+            }
+            if(!responseObject[department][space][date]) {
+                responseObject[department][space][date] = {};
+            }
+            if(!responseObject[department][space][date][key]) {
+                responseObject[department][space][date][key] = {};
+            }
+            responseObject[department][space][date][key] = alertObject[key];
+        }
+        res.status.send(responseObject);
     });
 });
 
