9be4c115317d2fdf048b501a358f1eb00a384b02
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-11-06 00:54:46 +0800

    更新問題

--- routes/notify.js
+++ routes/notify.js
@@ -5,7 +5,11 @@ router.get('/', function(req, res) {
     ref = req.database.ref('/notify');
     ref.once("value").then(function(snapshot) {
         let notifyObject = snapshot.val();
-        res.status(200).send(notifyObject);
+        if(notifyObject) {
+            res.status(200).send(notifyObject);
+        } else {
+            res.status(404).send({"message": "找不到通知"});
+        }
     });
 });
 
