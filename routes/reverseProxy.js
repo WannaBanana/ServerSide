da149c2d736478928abe997a2d8c579d6e44de9f
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-11-07 19:52:48 +0800

    新增反向代理試驗版

---
+++ routes/reverseProxy.js
@@ -0,0 +1,106 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+router.get('/door', function(req, res) {
+    var options = {
+        method: 'GET',
+        url: 'http://163.22.32.200:3000/door',
+        headers:
+        { 'Content-Type': 'application/json' },
+        body: req.body,
+        json: true
+    };
+
+    request(options, function (error, response, body) {
+        if (error) {
+            res.status(response.statusCode).send(response);
+            throw new Error(error);
+        }
+
+        res.status(response.statusCode).send(body);
+    });
+});
+
+router.post('/door', function(req, res) {
+    var options = {
+        method: 'POST',
+        url: 'http://163.22.32.200:3000/door',
+        headers:
+        { 'Content-Type': 'application/json' },
+        body: req.body,
+        json: true
+    };
+
+    request(options, function (error, response, body) {
+        if (error) {
+            res.status(response.statusCode).send(response);
+            throw new Error(error);
+        }
+
+        res.status(response.statusCode).send(body);
+    });
+});
+
+router.put('/door', function(req, res) {
+    var options = {
+        method: 'PUT',
+        url: 'http://163.22.32.200:3000/door',
+        headers:
+        { 'Content-Type': 'application/json' },
+        body: req.body,
+        json: true
+    };
+
+    request(options, function (error, response, body) {
+        if (error) {
+            res.status(response.statusCode).send(response);
+            throw new Error(error);
+        }
+
+        res.status(response.statusCode).send(body);
+    });
+});
+
+router.patch('/door', function(req, res) {
+    var options = {
+        method: 'PATCH',
+        url: 'http://163.22.32.200:3000/door',
+        headers:
+        { 'Content-Type': 'application/json' },
+        body: req.body,
+        json: true
+    };
+
+    request(options, function (error, response, body) {
+        if (error) {
+            res.status(response.statusCode).send(response);
+            throw new Error(error);
+        }
+
+        res.status(response.statusCode).send(body);
+    });
+});
+
+router.delete('/door', function(req, res) {
+    var options = {
+        method: 'DELETE',
+        url: 'http://163.22.32.200:3000/door',
+        headers:
+        { 'Content-Type': 'application/json' },
+        body: req.body,
+        json: true
+    };
+
+    request(options, function (error, response, body) {
+        if (error) {
+            res.status(response.statusCode).send(response);
+            throw new Error(error);
+        }
+
+        res.status(response.statusCode).send(body);
+    });
+});
+
+
+module.exports = router;
