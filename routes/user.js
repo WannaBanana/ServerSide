f7f76b601490343f6492283326c15dda9f127630
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-11-11 17:49:22 +0800

    hot fix

--- routes/user.js
+++ routes/user.js
@@ -18,7 +18,6 @@ router.get('/verify', function(req, res) {
                 responseData['未驗證'] = userObject;
             }
             res.status(200).send(responseData);
-            return;
         });
     });
 });
